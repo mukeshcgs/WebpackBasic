fd28741958c375905974a67af73c89534acdfb2b
Mukesh <mukeshcgs@gmail.com>
2018-12-15 00:00:54 +0530

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,7 @@ import 'babel-polyfill';
 let wordnikAPI = "https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=e126907909a400f77500606e74703c2e7003c5507165fc0c7";
 
 fetch(wordnikAPI)
-    .then(response => response.json())
+    .then(response => response.json()) 
     .then(json => {
         $(".word").text(json.word)
         console.log(json.word)
